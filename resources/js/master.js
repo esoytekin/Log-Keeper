e991abe12b36e3fd62d73aa921c55f68a208a0e0
Emrah Soytekin <emrahsoytekin@gmail.com>
2016-02-28 00:30:19 +0200

    new commit

--- resources/js/master.js
+++ resources/js/master.js
@@ -9,6 +9,43 @@ ko.bindingHandlers.fadeVisible = {
                         shouldDisplay() ? $(element).fadeIn() : $(element).fadeOut();
 		}
 };
+var file_ff = function(name, path, type){
+    var self = this;
+    self.name = name;
+    self.path = path;
+    self.type = type;
+}
+
+var itemGroup = function(name,fileList,taglist,description){
+    var self = this;
+    self.name = name;
+    self.fileList = fileList;
+    self.taglist = taglist;
+    self.description=description;
+} 
+var getFileList = function(name){
+    var fileList = new Array();
+    for (var i=0; i < 5; ++i) {
+        var fileType = i ==0 ? "pcap" : "txt";
+        var file_fx = new file_ff(name+"_"+i,"path_"+i,fileType);
+        fileList.push(file_fx);
+        
+    }
+    return fileList;
+
+}
+var getTagList = function(){
+    var tagList = new Array();
+    tagList.push("Success");
+    tagList.push("Transcoder");
+    tagList.push("Anonymous");
+    return tagList;
+
+}
+
+var getdescription = function(){
+    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque arcu vel odio dapibus blandit. Proin mauris nulla, rutrum ac hendrerit vitae, pulvinar eu sapien. Morbi imperdiet, sapien a placerat semper, sapien quam cursus mi, nec rhoncus nibh magna a enim. Vivamus metus arcu, tempus lobortis imperdiet et, ornare quis justo. Suspendisse potenti. Donec at lorem at elit elementum sodales. Fusce posuere mollis neque, sit amet aliquam ligula convallis eget. Sed sit amet justo at nulla consequat vulputate. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.";
+}
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
     var self = this;
@@ -16,38 +53,30 @@ function AppViewModel() {
     self.lastName = "Bertington";
     self.title = "LK";
 
-     self.people = ko.observableArray([
-        { name: 'First Item' },
-        { name: 'Second Item' },
-        { name: 'Third Item' },
-        { name: 'Fourth Item' },
-        { name: 'Fifth Item' },
-        { name: 'Sixth Item' },
-        { name: 'Seventh Item' },
-        { name: 'Eight Item' },
-        { name: 'Sixth Item' },
-        { name: 'Seventh Item' },
-        { name: 'Eight Item' },
-        { name: 'Sixth Item' },
-        { name: 'Seventh Item' },
-        { name: 'Eight Item' },
-        { name: 'Ninth Item' }
-    ]);
+    self.fileGroup = ko.observableArray(); 
+
+
+    var fileGroupItem = new itemGroup("First File", getFileList("First"),getTagList());
+    self.fileGroup.push(fileGroupItem);
+    fileGroupItem = new itemGroup("Second File", getFileList("Second"),undefined,getdescription());
+    self.fileGroup.push(fileGroupItem);
 
     self.selectedFile = ko.observable();
+    self.selectedSubFiles = ko.observableArray([]);
+    self.selectedTagList = ko.observableArray([]);
 
 
     self.pageNumber = ko.observable(0);
     self.nbPerPage = 10;
     self.totalPages = ko.computed(function() {
-        var div = Math.floor(self.people().length / self.nbPerPage);
-        div += self.people().length % self.nbPerPage > 0 ? 1 : 0;
+        var div = Math.floor(self.fileGroup().length / self.nbPerPage);
+        div += self.fileGroup().length % self.nbPerPage > 0 ? 1 : 0;
         return div;
     });
 
     self.paginated = ko.computed(function() {
         var first = self.pageNumber() * self.nbPerPage;
-        return self.people.slice(first, first + self.nbPerPage);
+        return self.fileGroup.slice(first, first + self.nbPerPage);
     });
     self.hasPrevious = ko.computed(function() {
         return self.pageNumber() !== 0;
@@ -77,9 +106,15 @@ function AppViewModel() {
     		self.selectedFile(''); 
     	}else{ 
             self.selectedFile(item); 
+            self.selectedSubFiles(item.fileList);
+            self.selectedTagList(item.taglist);
     	}
         //self.selectedFile(item);
     
+    }
+
+    self.closeFile = function(){
+        self.selectedFile('');
     }
     
 
